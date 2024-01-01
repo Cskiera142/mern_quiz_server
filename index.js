c4926690d38da0712f5a1dd7033b44c3017ed450
Kayce Arthur <cskiera142@gmail.com>
2024-01-01 16:50:41 -0700

    basic server setup

---
+++ index.js
@@ -0,0 +1,28 @@
+import express from "express";
+import morgan from "morgan";
+import cors from "cors";
+import { config } from "dotenv";
+
+const app = express();
+
+/** middleware */
+app.use(morgan("tiny"));
+app.use(cors());
+app.use(express.json());
+config();
+
+/** app port */
+const PORT = process.env.PORT || 3500;
+
+/** Routes */
+app.get("/", (req, res) => {
+  try {
+    res.json("Hello from the server");
+  } catch (error) {
+    res.json("error");
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
