856c37b62883dc5e2a39d598feb00855bf691ba2
Colton Skiera <cskiera142@gmail.com>
2024-01-02 05:23:26 -0700

    backend built, needs mongo

--- index.js
+++ index.js
@@ -2,31 +2,44 @@ import express from "express";
 import morgan from "morgan";
 import cors from "cors";
 import { config } from "dotenv";
+import router from "./router/route.js";
+
+/** import connection file */
+import connect from "./database/conn.js";
 
 const app = express();
 
-/** middleware */
+/** app middlewares */
 app.use(morgan("tiny"));
 app.use(cors());
 app.use(express.json());
 config();
-import router from "./router/route.js";
 
-/** app port */
-const PORT = process.env.PORT || 3500;
+/** appliation port */
+const port = process.env.PORT || 8080;
 
-/** Routes */
-
-app.use("/api", router); /**apis */
+/** routes */
+app.use("/api", router); /** apis */
 
 app.get("/", (req, res) => {
   try {
-    res.json("Hello from the server");
+    res.json("Get Request");
   } catch (error) {
-    res.json("error");
+    res.json(error);
   }
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+/** start server only when we have valid connection */
+connect()
+  .then(() => {
+    try {
+      app.listen(port, () => {
+        console.log(`Server connected to http://localhost:${port}`);
+      });
+    } catch (error) {
+      console.log("Cannot connect to the server");
+    }
+  })
+  .catch((error) => {
+    console.log("Invalid Database Connection");
+  });
